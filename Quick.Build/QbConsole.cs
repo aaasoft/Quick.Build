695df243dc4d8159669f6a37ea38d9fb63981e68
张鹏 <scbeta@qq.com>
2024-07-12 18:07:27 +0800

    增加QbConsole类

---
+++ Quick.Build/QbConsole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Quick.Build;
+
+public static class QbConsole
+{
+    public static void DisplaySameLineInConsole(string line)
+    {
+        Console.CursorLeft = 0;
+        Console.Write(string.Empty.PadRight(Console.WindowWidth - 1));
+        Console.CursorLeft = 0;
+        Console.Write(line);
+    }
+}
